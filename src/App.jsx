102054b168f1c3c2c89e7105d2d12d68d1fed6b1
Yuy-1 <DM-Yuy@proton.me>
2022-12-27 15:21:58 -0500

    add form/button tags within main div

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import { ArrowCircleRightIcon } from '@heroicons/react/solid'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -8,6 +8,16 @@ function App() {
     <main className="grid place-items-center min-h-screen bg-gradient-to-b from-slate-100 to-slate-200 dark:from-slate-800 dark:to-slate-900 text-slate-900 dark:text-slate-200">
       <div className="grid place-items-center gap-8 m-8">
         <h1 className="text-3xl sm:text=6xl font-bold text-center">What is your "One Thing"?</h1>
+        <form
+          className=""
+          //onSubmit
+        >
+          <input type="text"/>
+          <button>
+            <ArrowCircleRightIcon className="h-12 w-12" />
+          </button>
+        </form>
+
       </div>
 
     </main>
