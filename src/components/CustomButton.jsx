397d0484cbb651291dbd604e0f2c46209a53332b
Yuy-1 <DM-Yuy@proton.me>
2022-12-27 18:50:42 -0500

    wrap text in span with CSS styling add CheckCircleIcon inside button

--- src/components/CustomButton.jsx
+++ src/components/CustomButton.jsx
@@ -1,9 +1,13 @@
-import {ArrowRightCircleIcon} from "@heroicons/react/24/solid/index.js";
+//library imports
+import { CheckCircleIcon } from "@heroicons/react/solid/index.js"
 
 const CustomButton = ({text, handleCompletedThing}) => {
   return (
-    <button >
-      {text}
+    <button
+      className=""
+    >
+      <span className="pointer-events-none">{text}</span>
+      <CheckCircleIcon className="pointer-events-none w-12 h-12" />
     </button>
   )
 }
