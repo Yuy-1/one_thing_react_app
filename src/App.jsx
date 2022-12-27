7bb025d385339cf07314c7a1af6438459cbef3d2
Yuy-1 <DM-Yuy@proton.me>
2022-12-27 15:04:30 -0500

    set CSS classes for main, first div, h1 tags

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,10 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <main className="grid place-items-center min-h-screen bg-gradient-to-b from-slate-100 to-slate-500">
-      <h1>What is your "One Thing"?</h1>
+    <main className="grid place-items-center min-h-screen bg-gradient-to-b from-slate-100 to-slate-200 dark:from-slate-800 dark:to-slate-900 text-slate-900 dark:text-slate-200">
+      <div className="grid place-items-center gap-8 m-8">
+        <h1 className="text-3xl sm:text=6xl font-bold text-center">What is your "One Thing"?</h1>
+      </div>
 
     </main>
   )
