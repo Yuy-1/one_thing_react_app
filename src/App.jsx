475546cc6ddac0347d109802aa3dc9b67a855f59
Yuy-1 <DM-Yuy@proton.me>
2022-12-27 14:30:35 -0500

    initial setup

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <main className="grid place-items-center min-h-screen-bg-gradient-t-b from-slate-100 to-slate-500">
+    <main className="grid place-items-center min-h-screen bg-gradient-to-b from-slate-100 to-slate-500">
       <h1>What is your "One Thing"?</h1>
 
     </main>
