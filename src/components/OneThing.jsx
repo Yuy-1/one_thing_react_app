742baf50bf07fe86728e97ec268bff1c25f13bab
Yuy-1 <DM-Yuy@proton.me>
2022-12-27 18:25:40 -0500

    copy styling from h1 tag in app to OneThing.jsx add properties "thing, handleCompletedThing" to const OneThing

--- src/components/OneThing.jsx
+++ src/components/OneThing.jsx
@@ -1,9 +1,13 @@
 import {ArrowRightCircleIcon} from "@heroicons/react/24/solid/index.js";
 
-const OneThing = ({thing}) => {
+const OneThing = ({thing, handleCompletedThing}) => {
   return (
     <>
-      <p>{thing}</p>
+      <h1 className="text-3xl sm:text=6xl font-bold text-center">{thing}</h1>
+      <CustomButton
+        text="Mark Done"
+        handleCompletedThing={handleCompletedThing}
+      />
     </>
   )
 }
